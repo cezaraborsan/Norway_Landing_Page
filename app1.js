acd32d6e0e705cbe8fe6a943634fa81b0d54d259
Cezara Borsan <cezarabo@gmail.com>
2022-07-28 15:17:01 +0300

    More work on navbar and menu.

--- app1.js
+++ app1.js
@@ -1,9 +1,27 @@
 const menu = document.querySelector(".links");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-btn");
+const screenWidth = screen.width;
 
-hamburgerMenu.addEventListener("click", () => {
-  menu.style.transform = "translateX(0)";
-  overlay.style.display = "block";
-  console.log("clicked");
-});
+if (screenWidth < 1000) {
+  hamburgerMenu.addEventListener("click", () => {
+    menu.style.transform = "translateX(0)";
+    overlay.style.display = "block";
+    closeBtn.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    menu.style.transform = "translateX(100%)";
+    overlay.style.display = "none";
+    closeBtn.style.display = "none";
+  });
+
+  window.addEventListener("mouseup", function (event) {
+    if (event.target != menu) {
+      menu.style.transform = "translateX(100%)";
+      overlay.style.display = "none";
+      closeBtn.style.display = "none";
+    }
+  });
+}
